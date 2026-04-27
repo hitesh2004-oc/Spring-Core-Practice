e66a524096f655976cd13b853cbee0da0c7a0744
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-04-27 17:45:19 +0530

    Spring Core Practice

---
+++ 02SpringCoreXmlTypeDependency/src/main/java/com/prasoon/Service.java
@@ -0,0 +1,23 @@
+package com.prasoon;
+
+public class Service {
+	private Repository repo;
+
+	public Service() {
+		super();
+	}
+
+	public Repository getRepo() {
+		return repo;
+	}
+
+	public void setRepo(Repository repo) {
+		this.repo = repo;
+	}
+
+	@Override
+	public String toString() {
+		return "Service [repo=" + repo + "]";
+	}
+
+}
---
+++ 02SpringCoreXmlTypeDependency/src/main/java/com/prasoon/Test.java
@@ -0,0 +1,13 @@
+package com.prasoon;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+		   Service s = (Service)ac.getBean("sob");
+		   System.out.println(s);  
+	}
+
+}
